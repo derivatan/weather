20ee4c2056c24c0f67797e011ee924e87f09e0a9
derivatan <derivatan@gmail.com>
2021-03-28 20:48:26 +0200

    added server endpoint for locations and dummy data

--- server/index.js
+++ server/index.js
@@ -1,6 +1,15 @@
 const express = require('express')
 app = express()
 
+// TODO: These values should typically be more suitable place, like a database.
+const locations = [
+    {Id: 1, Name: "Stockholm", lat: 59.329323, lng: 18.068581},
+    {Id: 2, Name: "Malmö", lat: 55.604980, lng: 13.003822},
+    {Id: 3, Name: "Göteborg", lat: 57.696991, lng: 11.986500},
+    {Id: 4, Name: "Västerås", lat: 59.609901, lng: 16.544809},
+    {Id: 5, Name: "Trollhättan", lat:58.283741, lng: 12.288490},
+]
+
 app.use((request, response, next) => {
     response.header('Access-Control-Allow-Origin', request.get('origin'));
     response.header('Access-Control-Allow-Credentials', true);
@@ -12,6 +21,12 @@ app.use((request, response, next) => {
 app.get('/', (request, response) => {
     console.log('index endpoint reached')
     response.send("I don't know whether the weather will improve.")
+})
+
+app.get('/locations', (request, response) => {
+    response.json({
+        locations: locations
+    })
 })
 
 app.listen(process.env.PORT, () =>  {
