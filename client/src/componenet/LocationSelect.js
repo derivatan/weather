26653eac9068a95bf88b41a96593274f861b78e9
derivatan <derivatan@gmail.com>
2021-03-28 23:14:25 +0200

    Location select use property to return id

--- client/src/componenet/LocationSelect.js
+++ client/src/componenet/LocationSelect.js
@@ -8,20 +8,26 @@ export default class LocationSelect extends React.Component {
         this.state = {
             locations: []
         }
+
         axios.get(global.serverURL + '/locations').then(response => {
             this.setState({locations: response.data.locations})
-            console.log(response.data.locations)
         })
+
+        this.selectLocation = this.selectLocation.bind(this)
+    }
+
+    selectLocation(event) {
+        this.props.setLocation(event.target.value)
     }
 
     render() {
-        const options =  [<option value="">-</option>]
+        const options = [<option value="">-</option>]
         this.state.locations.forEach((location, i) => {
-            options.push(<option value={location.Id}>{location.Name}</option>)
+            options.push(<option key={location.Id} value={location.Id}>{location.Name}</option>)
         })
 
         return (
-            <select>
+            <select onChange={this.selectLocation}>
                 {options}
             </select>
         )
